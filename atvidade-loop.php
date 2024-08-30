dd0b67217e593cf14f6cd1d47e0612b89479bdcb
Messias38 <messiasserram@gmail.com>
2024-08-29 21:15:29 -0300

    Add files via upload

---
+++ atvidade-loop.php
@@ -0,0 +1,13 @@
+<?php
+$mensagem = "Estou aprendendo apredendo loopings no Aprender PHP";
+
+$i = 1;
+
+
+
+while ($i < 100){
+    $mensagem = "Estou aprendendo apredendo loopings no Aprender PHP";
+    echo $i. "<br>";
+$i++;
+}
+?>
